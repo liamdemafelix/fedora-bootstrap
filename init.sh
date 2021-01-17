c772a6daa59abd79026fde9d30d2485ae617b895
Liam Demafelix <hey@liam.ph>
2021-01-17 10:11:22 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run an update
+sudo dnf update -y
+
+# Install wmctrl
+sudo dnf install wmctrl -y
+
+# Install Docker and docker-compose
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable --now docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
